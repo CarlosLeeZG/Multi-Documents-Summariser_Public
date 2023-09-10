fa30535c0badc3d0ab451f951966fb333046fbca
CarlosLeeZG <134188381+CarlosLeeZG@users.noreply.github.com>
2023-09-10 13:05:22 +0800

    Update Multi_Docs_Summarizer_Public.py

--- Multi_Docs_Summarizer_Public.py
+++ Multi_Docs_Summarizer_Public.py
@@ -4,7 +4,6 @@ import os
 import json
 import langchain
 from langchain.document_loaders import UnstructuredFileIOLoader
-from unstructured.partition.pdf import partition_pdf
 from langchain.chains.summarize import load_summarize_chain
 from langchain import PromptTemplate
 from langchain.chat_models import AzureChatOpenAI
