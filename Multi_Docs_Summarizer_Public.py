be09117dbf30f791a294fd8f9b9ec9615d949169
CarlosLeeZG <134188381+CarlosLeeZG@users.noreply.github.com>
2023-09-10 12:25:26 +0800

    Update Multi_Docs_Summarizer_Public.py
    Add "project" as keyword argument to FeedbackCollector() as it is a required field for trubrics[streamlit]==1.6.0

--- Multi_Docs_Summarizer_Public.py
+++ Multi_Docs_Summarizer_Public.py
@@ -113,6 +113,7 @@ def main():
             st.download_button("Download", data = output, file_name = "download.txt", mime="txt/csv")
 
         collector = FeedbackCollector(
+        project="Multiple Documents Summariser"
         email=st.secrets['feedback_user'], 
         password=st.secrets['feedback_pass'])
 
