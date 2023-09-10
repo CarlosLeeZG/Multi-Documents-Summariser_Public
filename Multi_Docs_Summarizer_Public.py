19095dd0ef3dd3091965bf4c4060e4758807e659
CarlosLeeZG <134188381+CarlosLeeZG@users.noreply.github.com>
2023-09-10 11:08:53 +0800

    Update Multi_Docs_Summarizer_Public.py
    - Add progress bar
    - Add spinner
    - Add feedback
    - Use load() instead of load_split()
    - Change to GPT-4

--- Multi_Docs_Summarizer_Public.py
+++ Multi_Docs_Summarizer_Public.py
@@ -1,6 +1,8 @@
 # Imports the OpenAI API and Streamlit libraries.
 import streamlit as st
+import streamlit_survey as ss
 import os
+import json
 import langchain
 from langchain.document_loaders import UnstructuredFileIOLoader
 from langchain.chains.summarize import load_summarize_chain
@@ -12,7 +14,11 @@ from langchain.callbacks.base import BaseCallbackHandler
 from langchain.docstore.document import Document
 import re
 import io
+import docx
+import pandas as pd
+from trubrics.integrations.streamlit import FeedbackCollector
 
+# Create Progress Bar
 class ProgressBarHandler(BaseCallbackHandler):
     current_counter = 0
     total_counter = 1
@@ -22,11 +28,16 @@ class ProgressBarHandler(BaseCallbackHandler):
         total_counter=1
     ) -> None:
         self.total_counter = total_counter
-        self.progress_bar = st.progress(0, text=f"Total chunks: {str(total_counter)}")
+        self.progress_text = st.empty()
+        self.progress_bar = st.progress(0)
     def on_chain_end(self, outputs: Dict[str, Any], **kwargs: Any) -> Any:
         """Run when chain ends running."""
         self.current_counter += 1
-        self.progress_bar.progress(int((self.current_counter * 1.0 / (self.total_counter + 1)) * 100))  
+        progress_percentage = int((self.current_counter * 1.0 / (self.total_counter + 1)) * 100)  # show the progress bar meter moving
+        self.progress_bar.progress(progress_percentage)
+        self.progress_text.text(f"Total Chunks: {str(self.total_counter+1)}"   #show the number of chunks
+                                f"\nProgress: {progress_percentage}% ({str(self.current_counter)} / {str(self.total_counter+1)})")  # show the % progress
+  
         
         
 def main():
@@ -43,7 +54,8 @@ def main():
     Notes:
     - This application is a BETA version
     - Accuracy: Due to ongoing development and the nature of the AI language model, the results may generate inaccurate or misleading information
-    - Accountability: All output must be fact-checked, proof-read, and adapted as appropriate by officers for their work
+    - Accountability: All output m
+    ust be fact-checked, proof-read, and adapted as appropriate by officers for their work
     - Feedback: If you have any suggestion to improve this application, please provide them in the text box below """)
     agree = st.checkbox("I understand that I am responsible for the use of this tool as a productivity tool and that the app creator is not liable for the credibility of the results genereated.")
 #     """Returns `True` if the user had the correct password."""
@@ -54,7 +66,6 @@ def main():
         st.caption("AI-powered application that condense document texts into more concise and coherent summaries")
 
         max_summary_size = st.number_input('Max summary words(Indicative)', value=200, step=10, min_value=50)
-        num_summaries = st.slider("Number of Summaries (Note: more summaries requested, the longer the wait time)", min_value=1, max_value=3, step=1, value=1)
 
         # Add a file uploader widget
         multi_pdf = st.file_uploader("Choose a file", accept_multiple_files=True)
@@ -69,64 +80,51 @@ def main():
 
                 # Read the contents of the file
                 loader = UnstructuredFileIOLoader(file_like_object)
-                split_documents = loader.load_and_split()
+                split_documents = loader.load()
                 llm = AzureChatOpenAI(temperature=0, 
                     verbose=True, 
-                    deployment_name="gpt-35-turbo-16k"
+                    deployment_name="gpt-4"
                 )
 
-                template = """Summarise the following paper to about {word_limit} words: " + "paper: {text}"""
-                prompt = PromptTemplate(template = template, input_variables = ['text', 'word_limit'])
-
+                template = """Summarise the following document close to {max_summary_size} words, and capturing the main key points of the document.Ignore all footnote and references: file: {text}"""
+                prompt = PromptTemplate(template = template, input_variables = ['text', 'max_summary_size'])
                 chain = load_summarize_chain(llm, chain_type="refine", refine_prompt = prompt)
-
+                splitter = CharacterTextSplitter.from_tiktoken_encoder(chunk_size=5000, chunk_overlap=20)
+                split_documents = splitter.split_documents(document)
 
                 handler = ProgressBarHandler(total_counter=len(split_documents))
+                with st.spinner("Please wait, summarisation in process..."):
+                    summarize_text = chain.run({"input_documents": split_documents, "sentence_limit": sentence_limit}, callbacks=[handler])
+                    summarize_text_clean = summarize_text.replace("\n\n", " ")
 
-                summarize_text = chain({"input_documents": split_documents, "word_limit":max_summary_size}, return_only_outputs = True)['output_text']
-                summarize_text_clean = summarize_text.replace("\n\n", " ")
-
-                summaries = []
-
-
-
+               current_tries = 1
                 # Ensure text is less than word limit and not too far off from its limit
-                if len(re.findall(r'\w+', summarize_text_clean)) < max_summary_size and len(re.findall(r'\w+', summarize_text_clean)) > max_summary_size-50:
-                    summaries.append(summarize_text_clean)
-                else:
-                    splitter = CharacterTextSplitter.from_tiktoken_encoder(chunk_size=5000, chunk_overlap=20)
-                    split_texts = splitter.split_text(summarize_text_clean)
-                    split_documents = [Document(page_content=t) for t in split_texts]
-                    summarize_text_clean = chain({"input_documents": split_documents, "word_limit":max_summary_size}, return_only_outputs =True)['output_text']
-                    summaries.append(summarize_text_clean)
+                while len(re.findall(r'\w+', summarize_text_clean)) > max_summary_size and current_tries <= 5:
+                    with st.spinner('Please wait ahwile longer while the model optimise the summarised result...'):
+                        split_texts = splitter.split_text(summarize_text_clean)
+                        split_documents = [Document(page_content=t) for t in split_texts]
+                        summarize_text_clean = chain({"input_documents": split_documents, "sentence_limit": sentence_limit}, callbacks=[handler])['output_text']
+                        current_tries += 1
 
-                for i in range(num_summaries-1):
-                    summarize_text_clean2 = chain({"input_documents": split_documents, "word_limit":max_summary_size}, return_only_outputs = True)['output_text']
-                    while len(summarize_text_clean2.split()) == len(summaries[i].split()):
-                        summarize_text_clean2 = chain({"input_documents": split_documents, "word_limit":max_summary_size}, return_only_outputs = True)['output_text']
-                    summaries.append(summarize_text_clean2)
+                st.write(file.name, "(", len(summarize_text_clean.split()), "words )")
+                st.write(summarize_text_clean)
+                dict_ = {file.name: summarize_text_clean}
+                output = {}
+                output.update(dict_)
+    
+        # Require this to ensure proper formatting when save over as txt
+            output = '\n\n'.join(['%s: \n%s' % (key, value) for (key, value) in output.items()])
+            st.download_button("Download", data = output, file_name = "download.txt", mime="txt/csv")
 
+        collector = FeedbackCollector(
+        component_name="default",
+        email=st.secrets['feedback_user'], 
+        password=st.secrets['feedback_pass'])
 
-                st.write(pdf.name)
-                for index, summary in enumerate(summaries):
-                    index +=1
-                    name = [pdf.name]
-                    summarised = [summary]
-                    dict_ = dict(zip(name, summarised))
-                    output |= dict_
-                    st.write("Summary: ", index, "(", len(summary.split()), "words )")
-                    st.write(summary)
+        collector.st_feedback(
+        feedback_type="faces", model = 'document summarizer',
+        open_feedback_label="Provide additional feedback") 
 
-
-            # Require this to ensure proper formatting when save over as txt
-            output = '\n\n'.join(['%s:: %s' % (key, value) for (key, value) in output.items()])
-
-
-
-            st.download_button("Download", data = output, file_name = "download.txt", mime="text/csv")
-            st.button("Regenerate")
-
-        st.text_input("Please provide your feedback here")
 
 if __name__ == "__main__":
     main()
