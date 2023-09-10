c51431eba3bb34a92cbc268a749a0b693c85a4fa
CarlosLeeZG <134188381+CarlosLeeZG@users.noreply.github.com>
2023-09-10 14:01:12 +0800

    Update Multi_Docs_Summarizer_Public.py

--- Multi_Docs_Summarizer_Public.py
+++ Multi_Docs_Summarizer_Public.py
@@ -76,7 +76,7 @@ def main():
 
                 # Read the contents of the file
                 loader = UnstructuredFileIOLoader(file_like_object)
-                documents = loader.load()
+                document = loader.load()
                 llm = AzureChatOpenAI(temperature=0, 
                     verbose=True, 
                     deployment_name="gpt-4"
